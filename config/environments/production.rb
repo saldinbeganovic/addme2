5778cd8401241034fc009bc9a50925eea2525247
Saldin <64772544+saldinbeganovic@users.noreply.github.com>
2022-02-16 22:08:28 +0100

    hm

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_cable.url = "ws://addme-saldin.herokuapp.com//cable"
+config.action_cable.allowed_request_origins = ["https://addme-saldin.herokuapp.com", "addme-saldin.herokuapp.com"]
   # Code is not reloaded between requests.
   config.cache_classes = false
 config.reload_classes_only_on_chage = false
